ba17bd0fb9a0f995a4dd0b1c159eb30cc5303d83
Yogesh Kumar <yogeshkumar180592@gmail.com>
2017-01-29 01:23:05 +0530

    Initial Commit - Setting up the project

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './app/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    publicPath: '/public/',
+    noInfo: true,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        include: path.resolve(__dirname, 'app'),
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
